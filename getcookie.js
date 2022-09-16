764a71f5668afdd7c5ef651747ae272b4954485b
Softlyx <109867095+Softlyx@users.noreply.github.com>
2022-09-16 15:25:34 +0800

    Update getcookie.js

--- getcookie.js
+++ getcookie.js
@@ -1,7 +1,7 @@
 # 百度贴吧每日自动签到
-
+[rewrite_local]
 ^https?:\/\/(c\.tieba\.baidu\.com|180\.97\.\d+\.\d+)\/c\/s\/login url script-request-header https://raw.githubusercontent.com/Softlyx/QuantumultX/main/tieba.js
 ^https?:\/\/c\.tieba\.baidu\.com\/c\/s\/channelIconConfig url script-request-header https://raw.githubusercontent.com/Softlyx/QuantumultX/main/tieba.js
 ^https?:\/\/tiebac\.baidu\.com\/c\/u\/follow\/getFoldedMessageUserInfo url script-request-header https://raw.githubusercontent.com/Softlyx/QuantumultX/main/tieba.js
-
+[mitm]
 hostname = c.tieba.baidu.com
